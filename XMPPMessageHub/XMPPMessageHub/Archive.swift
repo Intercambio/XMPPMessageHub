eb4d57b24a42a9960905284ee938cc0076a33128
Tobias Kräntzer <info@tobias-kraentzer.de>
2016-11-20 12:50:33 +0100

    Initial Commit

---
+++ XMPPMessageHub/XMPPMessageHub/Archive.swift
@@ -0,0 +1,15 @@
+//
+//  Archive.swift
+//  XMPPMessageHub
+//
+//  Created by Tobias Kraentzer on 20.11.16.
+//  Copyright © 2016 Tobias Kraentzer. All rights reserved.
+//
+
+import Foundation
+import PureXML
+import CoreXMPP
+
+public class Archive {
+    
+}
---
+++ XMPPMessageHub/XMPPMessageHub/XMPPMessageHub.h
@@ -0,0 +1,19 @@
+//
+//  XMPPMessageHub.h
+//  XMPPMessageHub
+//
+//  Created by Tobias Kraentzer on 20.11.16.
+//  Copyright © 2016 Tobias Kraentzer. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for XMPPMessageHub.
+FOUNDATION_EXPORT double XMPPMessageHubVersionNumber;
+
+//! Project version string for XMPPMessageHub.
+FOUNDATION_EXPORT const unsigned char XMPPMessageHubVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XMPPMessageHub/PublicHeader.h>
+
+
