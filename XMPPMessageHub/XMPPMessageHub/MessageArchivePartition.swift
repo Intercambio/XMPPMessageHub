9b8b41ab6492b1d1d0620775ff0774d89fba6f83
Tobias Kräntzer <info@tobias-kraentzer.de>
2017-01-08 15:01:02 +0100

    Implementiert merging of partitions

--- XMPPMessageHub/XMPPMessageHub/MessageArchivePartition.swift
+++ XMPPMessageHub/XMPPMessageHub/MessageArchivePartition.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-struct MessageArchivePartition {
+struct MessageArchivePartition: Equatable {
     let first: MessageArchvieID
     let last: MessageArchvieID
     let timestamp: Date
@@ -16,4 +16,48 @@ struct MessageArchivePartition {
     let complete: Bool
     let archvieIDs: Set<MessageArchvieID>
     let before: MessageArchvieID?
+    
+    static func ==(lhs: MessageArchivePartition, rhs: MessageArchivePartition) -> Bool {
+        return
+            lhs.first == rhs.first &&
+            lhs.last == rhs.last &&
+            lhs.timestamp == rhs.timestamp &&
+            lhs.stable == rhs.stable &&
+            lhs.complete == rhs.complete &&
+            lhs.archvieIDs == rhs.archvieIDs &&
+            lhs.before == rhs.before
+    }
+}
+
+extension MessageArchivePartition {
+    func merge(_ other: MessageArchivePartition) -> (recent: MessageArchivePartition, other: MessageArchivePartition?) {
+        if self.archvieIDs.isDisjoint(with: other.archvieIDs) {
+            if let before = self.before, other.first == before {
+                return (self + other, nil)
+            } else if let before = other.before, self.first == before {
+                return (other + self, nil)
+            } else if timestamp > other.timestamp {
+                return (self, other)
+            } else {
+                return (other, self)
+            }
+        } else {
+            if timestamp > other.timestamp {
+                return (other + self, nil)
+            } else {
+                return (self + other, nil)
+            }
+        }
+    }
+    
+    static func +(lhs: MessageArchivePartition, rhs: MessageArchivePartition) -> MessageArchivePartition {
+        return MessageArchivePartition(
+            first: lhs.first,
+            last: rhs.last,
+            timestamp: lhs.timestamp,
+            stable: lhs.stable && rhs.stable,
+            complete: lhs.complete || rhs.complete,
+            archvieIDs: lhs.archvieIDs.union(rhs.archvieIDs),
+            before: rhs.before)
+    }
 }
--- XMPPMessageHub/XMPPMessageHubTests/MessageArchivePartitionTests.swift
+++ XMPPMessageHub/XMPPMessageHubTests/MessageArchivePartitionTests.swift
@@ -7,9 +7,102 @@
 //
 
 import XCTest
+@testable import XMPPMessageHub
 
 class MessageArchivePartitionTests: TestCase {
     
+    func testMergeNonIntersecting() {
+        
+        let partitionA = MessageArchivePartition(
+            first: "a",
+            last: "f",
+            timestamp: Date(timeIntervalSince1970: 100),
+            stable: true,
+            complete: false,
+            archvieIDs: ["a", "b", "c", "d", "e", "f"],
+            before: nil)
+        
+        let partitionB = MessageArchivePartition(
+            first: "h",
+            last: "l",
+            timestamp: Date(timeIntervalSince1970: 120),
+            stable: true,
+            complete: false,
+            archvieIDs: ["h", "i", "j", "k", "l"],
+            before: nil)
+        
+        var (recent, other) = partitionA.merge(partitionB)
+        XCTAssertEqual(recent, partitionB)
+        XCTAssertEqual(other, partitionA)
+        
+        (recent, other) = partitionB.merge(partitionA)
+        XCTAssertEqual(recent, partitionB)
+        XCTAssertEqual(other, partitionA)
+    }
     
+    func testMergeConsecutive() {
+        
+        let partitionA = MessageArchivePartition(
+            first: "a",
+            last: "f",
+            timestamp: Date(timeIntervalSince1970: 100),
+            stable: true,
+            complete: false,
+            archvieIDs: ["a", "b", "c", "d", "e", "f"],
+            before: "g")
+        
+        let partitionB = MessageArchivePartition(
+            first: "g",
+            last: "l",
+            timestamp: Date(timeIntervalSince1970: 120),
+            stable: true,
+            complete: false,
+            archvieIDs: ["g", "h", "i", "j", "k", "l"],
+            before: "x")
+        
+        
+        let (recentX, otherX) = partitionA.merge(partitionB)
+        XCTAssertNil(otherX)
+        XCTAssertEqual(recentX.first, "a")
+        XCTAssertEqual(recentX.last, "l")
+        XCTAssertEqual(recentX.before, "x")
+        XCTAssertEqual(recentX.timestamp, Date(timeIntervalSince1970: 100))
+        
+        let (recentY, otherY) = partitionB.merge(partitionA)
+        XCTAssertNil(otherY)
+        XCTAssertEqual(recentY, recentX)
+    }
+    
+    func testMergeIntersecting() {
+        
+        let partitionA = MessageArchivePartition(
+            first: "a",
+            last: "f",
+            timestamp: Date(timeIntervalSince1970: 100),
+            stable: true,
+            complete: false,
+            archvieIDs: ["a", "b", "c", "d", "e", "f"],
+            before: "g")
+        
+        let partitionB = MessageArchivePartition(
+            first: "d",
+            last: "i",
+            timestamp: Date(timeIntervalSince1970: 120),
+            stable: true,
+            complete: false,
+            archvieIDs: ["d", "e", "f", "g", "h", "i"],
+            before: "x")
+        
+        let (recentX, otherX) = partitionA.merge(partitionB)
+        XCTAssertNil(otherX)
+        XCTAssertEqual(recentX.first, "a")
+        XCTAssertEqual(recentX.last, "i")
+        XCTAssertEqual(recentX.before, "x")
+        XCTAssertEqual(recentX.timestamp, Date(timeIntervalSince1970: 100))
+        
+        let (recentY, otherY) = partitionB.merge(partitionA)
+        XCTAssertNil(otherY)
+        XCTAssertEqual(recentY, recentX)
+    }
     
 }
